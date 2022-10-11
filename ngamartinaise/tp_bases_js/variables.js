ff1286fea0b5ce7b39c7e36c035c0a91d4937974
Martinaise <Martinaisenga@gmail.com>
2022-10-11 16:56:33 +0200

    variablesfonction

---
+++ ngamartinaise/tp_bases_js/variables.js
@@ -0,0 +1,8 @@
+// je declarre 4 variables et je les affectent les valeurs
+var entier = 189;
+var chaine = '189';
+let booleen = true;
+let flottant = 42.42;
+// je declarre un tableau avec les types
+var array =[  entier,chaine,true, flottant];
+
