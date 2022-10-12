cb88a99b6126d54b114d7770d9ca86fcb724008c
Hajar AOUIZIR <hajar.aouizir1@gmail.com>
2022-10-12 11:58:11 +0200

    ex3 hjr

--- Hajar_AOUIZIR/tp_bases_js/variable.js
+++ Hajar_AOUIZIR/tp_bases_js/variable.js
@@ -1,3 +1,14 @@
+/*Exercice 1 :
+//fichier de rendu : bases/variables.js
+//Declarez les variables suivantes dans un fichier variables.js :
+//- La variable "entier", qui contiendra la valeur 189.
+//- La variable "chaine", qui contiendra la chaine de caracteres"189".
+//- La variable "booleen", qui contiendra la valeur vraie.
+//- La variable "flottant", qui contiendra la valeur 42.42
+//- Le tableau "array", qui contiendra les valeurs des variables precedentes */
+
+
+
 let entier = 189;
 let chaine = "189";
 let booleen = true;
