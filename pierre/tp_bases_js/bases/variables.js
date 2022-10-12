4f6ba248fdc6770ae41efc08d913abe6b8d32d2c
pierre <zend_zone@yahoo.com>
2022-10-12 15:03:39 +0200

    exercice3 et exercice4

--- pierre/tp_bases_js/bases/variables.js
+++ pierre/tp_bases_js/bases/variables.js
@@ -1,6 +1,6 @@
 // variables.js
 let entier= 189;
-let chaine= "189";
+let chaine= "190";
 let booleen= true;
 let flottant= 42.42;
 let array= [entier, chaine, booleen, flottant];
--- pierre/tp_bases_js/script.js
+++ pierre/tp_bases_js/script.js
@@ -1,2 +1,11 @@
+// scripts
 console.log("Hello World !");
-ma_fonction(entier, chaine, booleen, flottant,array);
+ma_fonction(entier, chaine, booleen, flottant, array);
+console.info("Exercice 3");
+console.log(exercice3_chaineVersEntier("parse chaineVersEntier ", chaine));
+console.log(exercice3_chaineVersFlottant("parse chaineVersFlottant ", flottant));
+console.log(exercice3_entierVersChaine("parse entierVersChaine ", entier));
+console.log(exercice3_flottantVersChaine("parse flottantVersChaine ", flottant));
+console.log(exercice3_booleenVersChaine("parse booleenVersChaine ", booleen));
+let retourexerc4= exercice4(array);
+console.log(retourexerc4);
