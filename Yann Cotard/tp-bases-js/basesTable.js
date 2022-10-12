ca9013d68233d374a421df22d60140f38e70139f
yannformation <cotardyann@orange.fr>
2022-10-12 14:59:41 +0200

    exercice 4 corrigé

---
+++ Yann Cotard/tp-bases-js/basesTable.js
@@ -0,0 +1,10 @@
+// Exercice 5 :
+// fichier de rendu : bases/tables.js
+// Ecrivez une fonction table() qui prend un chiffre en parametres, et qui affiche sa
+// table de multiplication  avec un calcul par ligne.. Ecrivez ensuite une fonction display_table(),
+// qui affichera a l'aide de table() les tables de multiplication de 1 a 10.
+
+function table(nb){
+    nb = 0;
+    
+}
