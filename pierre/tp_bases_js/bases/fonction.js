207bda68a0094dfa23e5d01b5dcadfd6ace91573
pierre <zend_zone@yahoo.com>
2022-10-12 11:24:39 +0200

    suppression 3 fichiers a la racine

---
+++ pierre/tp_bases_js/bases/fonction.js
@@ -0,0 +1,10 @@
+// fonction.js
+function ma_fonction(entier, chaine, booleen, flottant,array)
+{
+    console.log(entier);
+    console.log(chaine);
+    console.log(booleen);
+    console.log(flottant);
+    console.log(array);
+    return 0;
+}
---
+++ pierre/tp_bases_js/script.js
@@ -0,0 +1,2 @@
+console.log("Hello World !");
+ma_fonction(entier, chaine, booleen, flottant,array);
