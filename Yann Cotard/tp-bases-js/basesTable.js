f55075f3cfb6c1cade605f702eae5dea65523123
yannformation <cotardyann@orange.fr>
2022-10-12 16:21:46 +0200

    exercice 5 basesTable

--- Yann Cotard/tp-bases-js/basesTable.js
+++ Yann Cotard/tp-bases-js/basesTable.js
@@ -5,6 +5,19 @@
 // qui affichera a l'aide de table() les tables de multiplication de 1 a 10.
 
 function table(nb){
-    nb = 0;
-    
+
+    var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    for(let i=1; i<number.length+1; i++){
+  console.log(text = nb + "X" + [i] + "=" + nb*[i])}
+};
+table(5)
+
+function display_table(){
+   var nTable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+   for(let j =1; j<nTable.length+1; j++){
+        table([j]);
+   }
 }
+display_table()
