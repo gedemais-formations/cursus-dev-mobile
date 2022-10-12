fb33418c79971b2c4d81f37d362e98210c7d9bdf
yannformation <cotardyann@orange.fr>
2022-10-12 14:37:06 +0200

    exercice 4 corrigé

--- Yann Cotard/tp-bases-js/baseArray.js
+++ Yann Cotard/tp-bases-js/baseArray.js
@@ -4,20 +4,28 @@
 // Ecrivez ensuite une fonction qui prend array en parametre, et qui renvoie
 // un tableau contenant les valeurs de array converties en chaines de caracteres.
 
+let entier = 189;
+
+let chaine = "189";
+
+let booleen = true;
+
+let flottant = 42.42;
+
+var array = [entier, chaine, booleen, flottant];
+
 function convertVariableToString(array){
     
-    let entier = 189;
-
-    let chaine = "189";
-    
-    let booleen = true;
-    
-    let flottant = 42.42;
-    
-    var array = [entier, chaine, booleen, flottant];
-    return (array = [entier, chaine, booleen, flottant].toString())
+   
+    return [array[0].toString(),
+    array[1].toString(),
+    array[2].toString(),
+    array[3].toString()];
 
 }
-convertVariableToString();
-console.log(convertVariableToString());
-console.log(typeof convertVariableToString());
+convertVariableToString(array);
+console.log(convertVariableToString(array));
+console.log(typeof array[0].toString());
+console.log(typeof array[1].toString());
+console.log(typeof array[2].toString());
+console.log(typeof array[3].toString());
