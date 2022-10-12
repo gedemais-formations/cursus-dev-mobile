a6a261d65af87742a85c6ec9ddee8f006bb1405f
pierre <zend_zone@yahoo.com>
2022-10-12 15:04:38 +0200

    exercice3 et exercice4

---
+++ pierre/tp_bases_js/bases/array.js
@@ -0,0 +1,12 @@
+// array.js
+
+function exercice4(arr)
+{
+    let resultat= [];
+    console.info("dans exercice4");
+    resultat.push(arr[0]);
+    resultat.push(arr[1]);
+    resultat.push(arr[2]);
+    resultat.push(arr[3]);
+    return resultat;
+}
