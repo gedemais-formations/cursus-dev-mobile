8e084d6ac240ab4933debcd511c731f8f45e5ad8
pierre <zend_zone@yahoo.com>
2022-10-12 16:22:44 +0200

    exercice 5 terminé

--- pierre/tp_bases_js/script.js
+++ pierre/tp_bases_js/script.js
@@ -1,14 +1,15 @@
 // scripts : script principal pour tous les exercices
 console.log("Hello World !");
 ma_fonction(entier, chaine, booleen, flottant, array);
+// exercice3
 console.info("Exercice 3");
 console.log(exercice3_chaineVersEntier("parse chaineVersEntier ", chaine));
 console.log(exercice3_chaineVersFlottant("parse chaineVersFlottant ", flottant));
 console.log(exercice3_entierVersChaine("parse entierVersChaine ", entier));
 console.log(exercice3_flottantVersChaine("parse flottantVersChaine ", flottant));
 console.log(exercice3_booleenVersChaine("parse booleenVersChaine ", booleen));
+// exercice4
 let retourexerc4= exercice4(array);
 console.log(retourexerc4);
 // exercice5
 display_table();
-table();
