682b84e2167778b9b10ab5885f86b82d6982c60a
Martinaise <Martinaisenga@gmail.com>
2022-10-13 16:11:09 +0200

    fibonacci

---
+++ ngamartinaise/fibonacci.js
@@ -0,0 +1,18 @@
+// Exercie 7 :
+// fichier de rendu : bases/fibonacci.js
+// Ecrivez une fonction recursive qui prend en parametres un nombre, et qui renvoie
+// la valeur correspondant a cet index dans la suite de fibonacci. Ecrivez ensuite
+// une fonction qui affichera les 100 premieres valeurs de la suite de fibonacci.
+
+//                 index  f0  f1  f2  f3  f4  f5  f6  f7  f8 f9  f10  f11  f12  fn
+// a suite de fibonacci   0	  1   1   2	  3	  5	  8	 13	 21	 34	 55	  89   144  fn-1 + fn+1
+
+function recursive (nombre){
+    //nombre ici est un entier
+    console.log(recursive);
+    return nombre;
+
+    recursive
+
+
+}
