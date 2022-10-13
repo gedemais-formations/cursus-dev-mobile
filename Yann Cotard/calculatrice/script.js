7401cce4a268667aeaf3232b1e74faf4a6291dea
yannformation <cotardyann@orange.fr>
2022-10-13 15:39:32 +0200

    exercice 7 basesFibonacci

---
+++ Yann Cotard/calculatrice/script.js
@@ -0,0 +1,4 @@
+function calculatrice(form)
+{
+
+}
--- Yann Cotard/tp-bases-js/basesConditions.js
+++ Yann Cotard/tp-bases-js/basesConditions.js
@@ -7,7 +7,7 @@ function primeNumber(num) {
     if(num < 2) return false;
 
     for (var i = 2; i < num; i++) {
-        if(num%i==0)
+        if(num%i===0)
             return false;
     }
     return i
