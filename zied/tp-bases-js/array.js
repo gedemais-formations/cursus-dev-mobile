69d5bca3c64680396108a5262d7b97e27caf5d20
unknown <ziedmbarki77181@gmail.com>
2022-10-13 11:50:24 +0200

    exo4

--- zied/tp-bases-js/array.js
+++ zied/tp-bases-js/array.js
@@ -1,7 +1,17 @@
-var entier = 189 ;
-var chaine = "189" ;
-var booleen = true ;
-var flottant = 42.42 ;
-let array = [entier, chaine, booleen, flottant];
+
+function tableau(array)
+{
+  let tab = [ ];
+  tab[0] = array[0].toString();
+  tab[1] = array[1].toString();
+  tab[2] = array[2].toString();
+  tab[3] = array[3].toString();
+ 
+    return tab;
+}
 
 
+let v = tableau(array);
+console.log(v);
+
+console.log(tableau(array));
