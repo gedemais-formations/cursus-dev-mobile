78d157fb79701eb0f35d854b32d492359600f592
Cecilia-gr <seiika.8@gmail.com>
2022-10-13 16:22:16 +0200

    Calculatrice V2

--- Cecilia/calculatrice/script.js
+++ Cecilia/calculatrice/script.js
@@ -48,29 +48,72 @@ function calculs(op, a, b) {
     return val
 }
 
+function CreateNb(a,i){
+    let j=i;
+    let tmp = "";
+    while(!isNaN(a[j])){
+        tmp = tmp + a[j];
+        a.splice(j,1);  
+    }
+    tmp = parseFloat(tmp);
+    a.splice(i,0,tmp);
+    return a;
+}
+
+function table(a){
+    a = a.replace(/ /g,"");
+    a= a.split("");
+    let i=0;
+
+    while (i<a.length){ 
+
+        if(!isNaN(a[i])){
+            a[i]= parseFloat(a[i]);
+        }
+
+        if(!isNaN(a[i]) && !isNaN(a[i+1])){
+            a = CreateNb(a, i);
+        }
+        i++;
+    }
+    return a;
+}
+
+function prio(donnees) {
+    
+    for (let i = 0; i < donnees.length; i++) {
+       
+        if((donnees[i] == "*") || (donnees[i] == "/")){
+            let op = donnees[i];
+            let a = donnees[i-1];
+            let b =donnees[i+1];
+            let tmp = calculs(op, a, b);
+
+            donnees.splice(i-1,3);
+            donnees.splice(i-1,0,tmp);
+
+            i=0;
+        }
+    }
+        return donnees;
+}
+
+
+
 
 function calculatrice(form){
     let n;
     let donnees  = form.elements[0].value;
-    donnees =  donnees.split(" ");
+    donnees =  table(donnees);
+    donnees= prio(donnees);
 
     while (donnees.length > 1){
         let op = donnees[1];
-        console.log("op ="+op);
-
-        let a = parseInt(donnees[0],10);
-        console.log("a="+a);
-
-        let b = parseInt(donnees[2], 10);
-        console.log("b"+b);
-
-
+        let a = donnees[0];
+        let b = donnees[2];
         donnees.shift();
         donnees.shift();
         donnees.shift();
-        console.log(donnees);
-        console.log("longueur de donnes "+donnees.length);
-
         n = calculs (op, a, b);
         donnees.unshift(n);
     }
