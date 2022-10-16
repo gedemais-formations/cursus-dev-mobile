9c25629e60c257e3d4675d906950b5c5a05fd47f
gedemais-formations <demaison.geoffrey@gmail.com>
2022-10-16 23:35:40 +0200

    modify_html example modified

---
+++ geoffrey/tp_bases_js/fonction.js
@@ -0,0 +1,18 @@
+let entier = 189;
+let chaine = "189";
+let booleen = true;
+let flottant = 42.42;
+let array = [entier, chaine, booleen, flottant];
+
+function affiche(entier, chaine, booleen, flottant, array)
+{
+    console.log(entier);
+    console.log(chaine);
+    console.log(booleen);
+    console.log(flottant);
+    console.log(array);
+
+    return 0;
+}
+
+affiche(entier, chaine, booleen, flottant, array);
