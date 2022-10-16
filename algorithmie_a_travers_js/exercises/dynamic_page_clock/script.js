d551e40f3fc8d1b6af0a363c0dd641f495b18289
gedemais-formations <demaison.geoffrey@gmail.com>
2022-10-16 23:53:55 +0200

    clock TP ready

--- algorithmie_a_travers_js/exercises/dynamic_page_clock/script.js
+++ algorithmie_a_travers_js/exercises/dynamic_page_clock/script.js
@@ -1,7 +1,18 @@
+/* 
+    Créer un objet Clock avec une fonction constructeur prenant en paramètres 
+	une heure, une minute et une seconde précise, et qui les stockera avec trois
+    propriétés. L'objet doit comporter une methode "tick", permettant de faire
+    s'ecouler une seconde. Les heures et les minutes doivent suivre en consequence.
+    Clock doit également comporter une méthode "display", qui sera chargée d'afficher
+    l'heure sous forme lisible dans le paragraphe contenant l'id "hour" à chaque
+    seconde, avec le formatage de votre choix.
+*/
+
 clock = new Clock(0, 0, 0);
 
 setInterval(async function tick()
 {
 }, 1000);
-/* setInterval est la pour permettre au script de s'executer toutes les 1000ms
-apres sa premiere execution (dans l'en-tete html par exemple).*/
+/* setInterval est là pour permettre à la fonction tick() de s'executer toutes les 1000ms
+apres sa premiere execution (dans l'en-tete html par exemple). Vous pouvez modifier le
+délai de 1000ms pour faire vos tests.*/
