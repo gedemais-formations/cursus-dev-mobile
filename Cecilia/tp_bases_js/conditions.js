42083b1c8d164810637faed025deb5ee9ea2b398
Cecilia-gr <seiika.8@gmail.com>
2022-10-17 14:35:19 +0200

    Horloge

--- Cecilia/tp_bases_js/conditions.js
+++ Cecilia/tp_bases_js/conditions.js
@@ -2,7 +2,7 @@
 et etant superieur à 9. */
 
 function Nbpremier() {
-    let n = 9
+    let n = 2;
     let div = 0 
     while (n <= 100) {
         for (let i = 1; i <= n ; i++){
@@ -11,7 +11,7 @@ function Nbpremier() {
             }
         }
         
-        if(div == 2){
+        if(div == 2 && n > 9){
 
             console.log(n + " est un nombre premier");
         }
@@ -21,4 +21,25 @@ function Nbpremier() {
     }
 }
 
-Nbpremier();
+// Nbpremier();
+
+// ============== Correction ========
+
+function isPremier_downup(n) {
+    for (let i = 2; i < n; i++) { // On test tout les diviseurs de  2 a n puisque tout les nombres sont divisible par 1 et par eux eux même 
+        if (n%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function premier() {
+    for (let i = 1; i <= 100; i++) {
+        if (i > 9 && isPremier_downup(i)) {
+            console.log(i);
+        }
+    }
+}
+
+premier();
