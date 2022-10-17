71dc8674bb3d7ab7cbde95ec85ad7391f31f7ee3
Cecilia-gr <seiika.8@gmail.com>
2022-10-17 17:00:45 +0200

    Canvas

---
+++ Cecilia/jeu_de_la_vie/script.js
@@ -0,0 +1,4 @@
+let dessin = document.getElementById('dessin'); //  pour <canvas id="dessin"></canvas>
+let ctx = dessin.getContext('2d'); //ctx variable nommée  au pif, cts pour context
+ctx.fillStyle= "black";
+ctx.fillRect(10,10,10,5); // fillRect pour un rectangle
