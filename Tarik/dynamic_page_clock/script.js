d3cd13c9795bc01698c8ca43a08f59dcf2feae8a
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-10-18 16:44:03 +0200

    maj scrip avec ternaire

--- Tarik/dynamic_page_clock/script.js
+++ Tarik/dynamic_page_clock/script.js
@@ -8,36 +8,41 @@
     seconde, avec le formatage de votre choix.
 */
 function Clock(h, min, sec) {
-    this.heures = h,
-    this.minutes = min,
-    this.secondes = sec,
-    this.tick = () => {
-        this.secondes++;
-        if (this.heures == 23 && this.minutes == 59 && this.secondes > 60) {
-            this.secondes, this.heures, this.minutes = 0;
-        }else if (this.secondes > 60 ) {
-            this.secondes = 0;
-            this.minutes++;
-            if (this.minutes > 60) {
-                this.minutes = 0;
-                this.heures++;
-            }
-        }
-    },
-    this.display = () => {
-        document.getElementById('hour').innerHTML = 
-            `Il est : ${this.heures} h ${this.minutes} min et ${this.secondes} sec !`;
-    }
+  (this.heures = h),
+    (this.minutes = min),
+    (this.secondes = sec),
+    (this.tick = () => {
+      this.secondes++;
+      if (this.secondes == 60) {
+        this.secondes = 0;
+        this.minutes++;
+      }
+      if (this.minutes == 60) {
+        this.minutes = 0;
+        this.heures++;
+      }
+      if (this.heures == 24) {
+        this.heures = 0;
+      }
+    }),
+    (this.display = () => {
+      let newHeures = this.heures < 10 ? "0" + this.heures : this.heures;
+      let newMinutes = this.minutes < 10 ? "0" + this.minutes : this.minutes;
+      let newSecondes =
+        this.secondes < 10 ? "0" + this.secondes : this.secondes;
+
+      document.getElementById(
+        "hour"
+      ).innerHTML = ` ${newHeures} : ${newMinutes} : ${newSecondes}`;
+    });
 }
 
-let clock = new Clock(0, 0, 58);
+let clock = new Clock(9, 50, 0);
 
-
-
-setInterval(async function tick(){
-    clock.tick()
-    clock.display()
-}, 1000);
+setInterval(async function tick() {
+  clock.tick();
+  clock.display();
+}, 1);
 /* setInterval est là pour permettre à la fonction tick() de s'executer toutes les 1000ms
 apres sa premiere execution (dans l'en-tete html par exemple). Vous pouvez modifier le
 délai de 1000ms pour faire vos tests.*/
