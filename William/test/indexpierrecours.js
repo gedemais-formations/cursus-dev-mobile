26ff59e3c0e660cbe19cfcbf57ee9bb98f4d00d2
william9942 <williamvvv333@gmail.com>
2022-10-19 10:47:28 +0200

    finition exo

---
+++ William/test/indexpierrecours.js
@@ -0,0 +1,102 @@
+
+//index.js
+
+
+
+//// Les fonctions
+/*
+C
+JS
+PHP
+C#
+JAVA
+*/
+
+function addition(a, b) {
+    return a + b;
+    
+    }
+
+function xyz(p1, p2){
+    return = p1 +p2
+
+}
+
+
+    function abcd(pa3, pa4){
+let re
+//faire le calcul
+re = addition(pa3, pa4);
+return = re
+
+    }
+
+function nom_de_fonction(part1, part2, part3) {
+let resultat;
+/*
+//le code de l'exercice
+*/
+//return resultat = par1 + par2 + par3 + par4
+// resultat = addition (par1, par2);
+let resultat1 = addition(par1 + part2, par3 + par4);
+let resultat3 = addition(par3 + par4,0);
+let resultat4 = addition(par3 + par4, 0);
+let resultat2 = resultat3+ resultat4;
+
+let resultat5 = addition(par1, par2) + addition(par3, par4);
+let resultat6 = addition (xyz(par1, par2)
++ abcd (par3, par4));
+let resultat8 = (addition(par1, par2), addition(par3, par4));
+}
+
+//utilisation de la fctn
+nom_de_fonction();
+
+////Revision LES FONCTIONS
+let mon_resultat;
+mon_resultat = nom_de_fonction(4, -1, 2, 6);
+console.log(mon_resultat);
+
+
+//2
+//POO
+//OOP= Object Operating Programming
+
+let objet_pour_donnees = {
+    nom: "Deresmes",
+    prenom: "Pierre",
+    adresse: "nom adresse",
+};
+
+/*
+let xxxx= ?????;
+let yyy;
+yyy= ????????;
+*/
+function Vehicule(){
+    /// Constructeur de ma classe générale
+
+    //un constructeur ne retourne jamais rien
+    //let res;
+    // res= ????
+    //return res;
+    this.nom_du_vehicule = par_nom; ///propriété ou attribut
+}
+
+let voiture = new Vehicule("Renault 2008");
+/// dans voiture, il y un objet qui est une instance de vehicule
+let velo = new Vehicule("Decathlon");
+/// dans velo, il y a un objet qui est une instance de vehicule
+let bus = new Vehicule("Man");
+/// dans bus, il y a un objet qui est une instance de vehicule
+let ratp_bus_ligne156 = 
+[bus, new Vehicule(), new Vehicule(), new Vehicule()];
+// let truc= Vehicule(); // pas de POO
+let tableau_entier = [11, 5, -5, 4]; // [0]==> 11 [1]==> 5 [2]==> -5 [3]==> 4
+let tableau_chaine = ["bon", "jour", "geoffrey"];
+let tour_de_france = [new Vehicule(), new Vehicule(), new Vehicule()];
+
+//new: constructeur
+
+
+
