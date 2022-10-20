2ab8b04e73cfca28bf1630d8af68ab87a783474a
denisalarcon <romero270814@gmail.com>
2022-10-20 17:22:22 +0200

    el juego de la vida (count_neigbours)

--- denis/jeu_de_la_vie/main.js
+++ denis/jeu_de_la_vie/main.js
@@ -1,10 +1,10 @@
 const alive_color = "black";
 const dead_color = "white";
 
-const cell_size =8;
+const cell_size =20; // normalement c'est 8
 
-const width = 50;
-const heigth = 50;
+const width = 4; //normalement cest 50
+const heigth = 3; // 50 aussi 
 
 const px_width = width * cell_size;
 const px_heigth = heigth * cell_size;
@@ -17,10 +17,22 @@ const board = board_canvas.getContext('2d');
 
 let array =[]; // model data structure
 
-function getRandomInt(max)
+function udateVieuw()
  {
-    return Math.floor(Math.random() * max)
-  };
+    board.fillStyle = alive_color;
+    for (let x =0; x < width; x++)
+     for ( let y = 0; y < heigth; y++)
+     //
+        if (array[x][y] == true)
+        board.fillRect (x *cell_size, y * cell_size, cell_size, cell_size);
+     }
+    
+ //
+ function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+//
 function draw_bakground()
 {
     board_canvas.width = px_width; // largeur en pixels du canvas
@@ -48,28 +60,74 @@ console.log (px_heigth);
      
     board.stroke(); // affiche le resultat a l'ecran
     
-    function generate_matrix()
-    {
-        for (let x = 0; x < width; x++){
-
-            array.push([]);
-
-            for (let y = 0; y < heigth; y++)
-            {
-
-                array[x].push(getRandomInt(100) < filling_rate);
-            } 
-        }
-        console.log(array);
-    }
+   
     generate_matrix();
 }
+function generate_matrix()
+{
+    array = [];
+    for (let x = 0; x < width; x++){
 
+        array.push([]);
+
+        for (let y = 0; y < heigth; y++)
+        {
+
+            array[x].push(getRandomInt(100) < filling_rate);
+        } 
+    }
+    console.log(array);
+}
+/* count_neighbours
+Compte le nombre de voisins vivants d'une cellule.
+*/
+function count_neighbours_joffrai(x, y)
+{
+
+    let result = 0;
+    
+    return result;
+    
+}
+/////////////////
+function count_neighbours(x, y)
+{
+    let cpt_vivant = 0;
+    for(let line =0; line<3; line++){
+        
+    for(let col =0 ; col< 3; col++){
+      
+        if(array [col][line]== true)
+        {
+           cpt_vivant++; 
+    }
+}
+    // 
+    }
+    return cpt_vivant;
+}
+
+/* update_model
+Itere sur array pour appliquer les regles du jeu de la vie sur chacune des cellules du board.
+(Creer la fonction count_neighbours pour compter les voisins d'une cellule)
+Si une cellule vivante a moins de 2 voisins, elle meure de solitude
+Si une cellule vivante a plus de 3 voisins, elle meure de surpopulation
+Si une cellule morte a 3 voisins, elle devient vivante
+Si une cellule a 2 voisins, elle conserve son etat actuel*/
+
+function update_model()
+{
+     
+}
+////////////////////////////////////////
 
 setInterval(async function tick() {
-   console.log("Hello");
-    draw_bakground();
+   console.log();
 
+   generate_matrix(); 
+    draw_bakground();
+    udateVieuw ();
+    
 }, 1000);
 /* setInterval est la pour permettre au script de s'executer toutes les 1000ms
 apres sa premiere execution (dans l'en-tete html par exemple).*/
